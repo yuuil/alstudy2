dc22d8248b1e739285bb2ef9203694b72d790cec
yuuil <yeyeah_1@daum.net>
2020-03-28 17:30:22 +0900

    BOJ2609 최대공약수와 최소공배수 재풀이

---
+++ Online/BOJ2609 최대공약수와 최소공배수.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b) {
+  return b == 0 ? a : gcd(b, a%b);
+}
+
+int lcm(int a, int b) {
+  return a * b / gcd(a,b);
+}
+
+int main() {
+  int a,b;
+  cin>>a>>b;
+  cout<<gcd(a,b)<<"\n"<<lcm(a,b)<<"\n";
+  return 0;
+}
