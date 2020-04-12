20e0536d8c9ccbabe9f81cc892059fa4d73cb796
yuuil <yeyeah_1@daum.net>
2020-04-12 21:12:49 +0900

    BOJ 2291 Sequence 문제풀이

--- Online/BOJ2291 Sequence.cpp
+++ Online/BOJ2291 Sequence.cpp
@@ -1,3 +1,8 @@
+/*
+dp[i][j][k] => 길이가 i이고, 마지막 수가 j이면서 합이 k인 수열의 개수
+구해야 하는 수열의 번호가 현재까지 합쳐진 것보다 크다면 누적된 수열 개수 뺌
+현재 채우는 수가 구해야 하는 수열의 번호보다 작다면 해당 수열을 이루는 수임
+*/
 #include <iostream>
 #include <cstring>
 using namespace std;
@@ -8,7 +13,7 @@ int dfs(int idx, int pre, int r) {
     if(ret!=-1) return ret;
     ret=0;
     for(int i=pre; i<=M; i++) {
-        if(i*(N-idx)<=r-i) ret+=dfs(idx+1,i,r-i);
+        if(i*(N-idx)<=r-i) ret+=dfs(idx+1,i,r-i); //현재에서 i를 채우고 남은 합이 남은 길이를 i로 채우는 것보다 크거나 같으면 수열의 개수 추가
     }
     return ret;
 }
