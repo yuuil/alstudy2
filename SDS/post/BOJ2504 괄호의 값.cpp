24a0718e3d0800f8295e542b610e232b060920fa
yuuil <yeyeah_1@daum.net>
2020-04-17 22:23:16 +0900

    BOJ2504 괄호의 값 문제풀이

---
+++ SDS/post/BOJ2504 괄호의 값.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+bool err;
+long long ans;
+int temp = 1;
+string input;
+stack<char> s;
+int main() {
+  cin>>input;
+  for(int i=0; i<input.size(); i++) {
+    if(input[i]=='('||input[i]=='[') {
+      s.push(input[i]);
+      temp *= (input[i]=='(' ? 2 : 3);
+    }
+    else if((input[i]==')'&&(s.empty()||s.top()!='('))||(input[i]==']'&&(s.empty()||s.top()!='['))) {
+      err=1;
+      break;
+    } else if(input[i]==')') {
+      if(input[i-1]=='(') ans+=temp;
+      s.pop();
+      temp/=2;
+    } else if(input[i]==']') {
+      if(input[i-1]=='[') ans+=temp;
+      s.pop();
+      temp/=3;
+    }
+  }
+  cout<<(err||!s.empty()?0:ans);
+  return 0;
+}
